9df2a9311fc3d9e11fb8409146aace31a2b06cdf
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-25 16:57:16 +0100

    Modifico Snack 2

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -1,8 +1,7 @@
 // Variabile per il div
 let container = document.getElementById('container');
 
-let arrayClub = [
-    {
+let arrayClub = [{
         'nome': 'Juventus',
         'puntifatti': 0,
         'fallisubiti': 0
@@ -40,13 +39,18 @@ let arrayClub = [
 ];
 
 // Creo numeri random per puntifatti e fallisubiti
+container.innerHTML +=`<h1>Array Iniziale con numeri random assegnati`
 for(let i = 0; i < arrayClub.length; i++){
 
     arrayClub[i].fallisubiti = numRandom1toX(200);
     arrayClub[i].puntifatti = numRandom1toX(100);
 
+    let{nome, puntifatti, fallisubiti} = arrayClub[i];
+
+    container.innerHTML +=`<div>Nome squadra: ${nome} Falli Subiti: ${fallisubiti} Punti: ${puntifatti}</div>`
 }
 
+container.innerHTML +=`<h2>Nuovo array</h2>`
 // Array che conterra i nuovi oggetti con nome e falli subiti
 let newArray = [];
 
@@ -55,20 +59,51 @@ for(let y = 0; y < arrayClub.length; y++){
 
     let {nome, fallisubiti} = arrayClub[y];
 
-    let newOne = {nome, fallisubiti};
+    newArray.push(
+        {
+            nome,
+            fallisubiti
+        }
+    )
     
-    newArray.push(newOne);
 }
 
-console.log( newArray )
+// Stampo i nuovi oggetti
+for(let z = 0; z < arrayClub.length; z++){
+
+    let {nome, fallisubiti} = newArray[z]
+
+    container.innerHTML += `<div>Nome squadra:${nome} Falli Subiti:${fallisubiti}</div>`
+    
+}
 
 
+
+
+
+// let newArray = [];
+
+// for (let i = 0; i < arrayClub.length; i++) {
+
+//     let {
+//         puntifatti,
+//         fallisubiti
+//     } = arrayClub[i];
+
+//     newArray.push({
+//         ...arrayClub[i],
+//         puntifatti: numRandom1toX(100),
+//         fallisubiti:numRandom1toX(200)
+//     })
+
+// }
+// console.log(newArray)
 // Funzione che crea un num random da 0 a x
-function numRandom1toX(x){
+function numRandom1toX(x) {
 
     let num;
 
-    num = Math.floor( ( Math.random() * x ) + 1 );
+    num = Math.floor((Math.random() * x) + 1);
 
     return num;
 
